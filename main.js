d651c846f549898f2cbe0809abb8f75dcbac4c35
Olimxoja77 <odilxojaevolimxoja@gmail.com>
2024-11-15 19:50:27 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let text =document.querySelector('h1')
+let a =prompt('Ismingizni kiriting')
+let b =prompt('Familiyangizni kiriting')
+let c =prompt('Parol kiriting')
+let x =Math.floor(Math.text())
+text.innerHTML =x;
